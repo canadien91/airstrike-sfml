7886f76d36cf67a0ae50b5d2d0cca0e7474c6ce7
Alexander Nefedov <nefedov.alexander91@yandex.com>
2015-06-29 21:23:14 +0500

    Integration of AWorld class into AGame class

--- airstrike/Game/Game.cpp
+++ airstrike/Game/Game.cpp
@@ -1,28 +1,16 @@
 
 #include "Game.h"
 
-const float     AGame::PLAYER_SPEED     = 200.0f;
 const sf::Time  AGame::TIME_PER_FRAME   = sf::Time( sf::seconds( 1.0f / 60.0f ) );
 
 AGame::AGame() :
-window( sf::VideoMode( 640, 480 ), "SFML Airstrike" ) {
+window( sf::VideoMode( 640, 480 ), "SFML Airstrike" ),
+world( this->window ) {
     this->window.setVerticalSyncEnabled( true );
-    this->texture                   = sf::Texture();
-    this->player                    = sf::Sprite();
     this->font                      = sf::Font();
     this->statistics_text           = sf::Text();
     this->statistics_update_time    = sf::Time();
     this->statistics_num_frames     = 0;
-    this->is_moving_up              = false;
-    this->is_moving_down            = false;
-    this->is_moving_left            = false;
-    this->is_moving_right           = false;
-
-    if ( !this->texture.loadFromFile( "Media/Textures/Eagle.png" ) ) {
-        // Handle loading error;
-    }
-    this->player.setTexture( this->texture );
-    this->player.setPosition( 100.0f, 100.0f );
 
     this->font.loadFromFile( "Media/Sansation.ttf" );
     this->statistics_text.setFont( font );
@@ -53,30 +41,13 @@ AGame::ProcessEvents() {
     while ( this->window.pollEvent( event) ) {
         if ( event.type == sf::Event::Closed ) {
             this->window.close();
-        } else if ( event.type == sf::Event::KeyPressed ) {
-            this->HandlePlayerInput( event.key.code, true );
-        } else if ( event.type == sf::Event::KeyReleased ) {
-            this->HandlePlayerInput( event.key.code, false );
         }
     }
 }
 
 void
 AGame::Update( const sf::Time& dt ) {
-    sf::Vector2f movement( 0.0f, 0.0f );
-    if ( this->is_moving_up ) {
-        movement.y -= PLAYER_SPEED;
-    }
-    if ( this->is_moving_down ) {
-        movement.y += PLAYER_SPEED;
-    }
-    if ( this->is_moving_left ) {
-        movement.x -= PLAYER_SPEED;
-    }
-    if ( this->is_moving_right ) {
-        movement.x += PLAYER_SPEED;
-    }
-    this->player.move( movement * dt.asSeconds() );
+    this->world.Update( dt );
 }
 
 void
@@ -99,20 +70,13 @@ AGame::UpdateStatistics( const sf::Time& dt ) {
 void
 AGame::Render() {
     this->window.clear();
-    this->window.draw( this->player );
+    this->world.Draw();
+
+    this->window.setView( this->window.getDefaultView() );
     this->window.draw( this->statistics_text );
     this->window.display();
 }
 
 void
 AGame::HandlePlayerInput( const sf::Keyboard::Key& key, const bool is_pressed ){
-    if ( key == sf::Keyboard::W ) {
-        this->is_moving_up = is_pressed;
-    } else if ( key == sf::Keyboard::S ) {
-        this->is_moving_down = is_pressed;
-    } else if ( key == sf::Keyboard::A ) {
-        this->is_moving_left = is_pressed;
-    } else if ( key == sf::Keyboard::D ) {
-        this->is_moving_right = is_pressed;
-    }
 }
--- airstrike/Game/Game.h
+++ airstrike/Game/Game.h
@@ -2,35 +2,24 @@
 #ifndef __GAME_H__
 #define __GAME_H__
 
-#include <string>
+#include <World/World.h>
 
-#include <SFML/Graphics.hpp>
-
-#include "ResourceHolder/ResourceIdentifiers.h"
-
-class AGame {
+class AGame : private sf::NonCopyable {
 public:
     AGame();
 
     void Run();
 
 private:
-    static const float      PLAYER_SPEED;
     static const sf::Time   TIME_PER_FRAME;
 
     sf::RenderWindow        window;
-    sf::Texture             texture;
-    sf::Sprite              player;
-    sf::Font                font;
+    AWorld                  world;
 
+    sf::Font                font;
     sf::Text                statistics_text;
     sf::Time                statistics_update_time;
     std::size_t             statistics_num_frames;
-
-    bool                    is_moving_up;
-    bool                    is_moving_down;
-    bool                    is_moving_left;
-    bool                    is_moving_right;
 
     void                    ProcessEvents();
     void                    Update( const sf::Time& dt );
